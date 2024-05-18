a7331c10025b4d26043ce2d8a958f49063f1ec56
Giacomo Bergami <35457258+gyankos@users.noreply.github.com>
2024-05-18 23:24:51 +0100

    Update my_unordered_set.h

--- include/my_unordered_set.h
+++ include/my_unordered_set.h
@@ -2,7 +2,7 @@
  * my_unordered_set.h
  * This file is part of DECLAREd
  *
- * Copyright (C) 2023 - Anonymous Ⅳ
+ * Copyright (C) 2023 - Giacomo Bergami
  *
  * DECLAREd is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
