7590a45a299f8693043f2e46dc4c08b1dfef2c1c
Giacomo Bergami <35457258+gyankos@users.noreply.github.com>
2024-05-18 23:24:02 +0100

    Update declare_to_lydialtlf.h

--- include/declare_to_lydialtlf.h
+++ include/declare_to_lydialtlf.h
@@ -2,7 +2,7 @@
  * declare_to_lydialtlf.h
  * This file is part of DECLAREd
  *
- * Copyright (C) 2023 - Anonymous Ⅳ
+ * Copyright (C) 2023 - Giacomo Bergami
  *
  * <program name> is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
